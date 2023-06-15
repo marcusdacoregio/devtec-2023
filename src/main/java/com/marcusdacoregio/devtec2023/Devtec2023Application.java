2c8387d9106522ee155c5a5a226eb1d0564ad241
Marcus Da Coregio <marcusdacoregio@gmail.com>
2023-06-15 09:09:54 -0300

    Initial commit

---
+++ src/main/java/com/marcusdacoregio/devtec2023/Devtec2023Application.java
@@ -0,0 +1,13 @@
+package com.marcusdacoregio.devtec2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devtec2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devtec2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcusdacoregio/devtec2023/Devtec2023ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.marcusdacoregio.devtec2023;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devtec2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
